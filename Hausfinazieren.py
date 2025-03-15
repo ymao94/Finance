0945709d65fd843296e542be79e112e56703bb6a
ymao94 <yybmao@gmail.com>
2025-03-15 22:45:27 +0100

    Deployed the read tax model instead of just top tax rate

--- Hausfinazieren.py
+++ Hausfinazieren.py
@@ -7,10 +7,9 @@ Alle Daten spezielle zur Immobile in Frage sind in der main Funktion einzutragen
 __author__ = "Yuanyuan Mao"
 __version__ = "1.0"
 
-STEUER = 0.06 # Grunderwerbsteuer in Hessen
+STEUER = 0.065 # Grunderwerbsteuer in Hessen
 MARKLER = 0.0357 # Normaler Maklerprovision in Hessen
 NOTAR = 0.015 # Durchschnittliche Notarkosten in Hessen
-EINKOMMENSTEUER = 0.42 # Spitzensteuersatz in Deutschland
 
 def nebenkosten(kaufpreis):
     return kaufpreis * (STEUER + MARKLER + NOTAR)
@@ -30,13 +29,41 @@ def abschreibung(kaufpreis, baujahr):
     if baujahr > 2022:
         return kaufpreis / 100 * 3
     
-def cashflow(kredit, miete, monatliche_rate, zins, abschreibung):
+def einkommensteuer(zu_versteuerndes_einkommen):
+    if zu_versteuerndes_einkommen <= 12096:
+        return 0.0    
+    elif zu_versteuerndes_einkommen <= 17443:
+        y = (zu_versteuerndes_einkommen - 12096) / 10000
+        return (932.3 * y + 1400) * y
+    elif zu_versteuerndes_einkommen <= 68480:
+        y = (zu_versteuerndes_einkommen - 17443) / 10000
+        return (176.64 * y + 2397) * y + 1015.13
+    elif zu_versteuerndes_einkommen <= 277825:
+        return 0.42 * zu_versteuerndes_einkommen - 10911.92
+    else:
+        return 0.45 * zu_versteuerndes_einkommen - 19246.67
+    
+def steuer2(zu_versteuerndes_einkommen):
+    steuer = 0
+    if zu_versteuerndes_einkommen <= 12096:
+        return steuer
+    if zu_versteuerndes_einkommen <= 17443-12906:
+        y = (zu_versteuerndes_einkommen - 12096) / 10000
+        return (932.3 * y + 1400) * y
+    steuer += (17443 - 12096) / 10000 * (932.3 * (17443 - 12096) / 10000 + 1400)
+    if zu_versteuerndes_einkommen <= 68480:
+        y = (zu_versteuerndes_einkommen - 17443) / 10000
+        return steuer + (176.64 * y + 2397) * y + 1015.13
+    if zu_versteuerndes_einkommen <= 277825:
+        return 0.42 * zu_versteuerndes_einkommen - 10911.92
+    else:
+        return 0.45 * zu_versteuerndes_einkommen - 19246.67
+    
+def cashflow(kredit, miete, monatliche_rate, zins, abschreibung, gehaltA, gehaltB):
     jahr = 1
     monatlicher_zins = (1 + zins)**(1/12) - 1 
-    monatliche_abschreibung = abschreibung / 12
     while True: # Endlosschleife, bis Kredit abbezahlt ist
         # Initialisierung für jedes Jahr
-        netto_miete = 0
         steuerersparnis = 0
         zinsen_bezahlt = 0
         tilgung_bezahlt = 0
@@ -50,33 +77,37 @@ def cashflow(kredit, miete, monatliche_rate, zins, abschreibung):
             if kredit <= 0: # Wenn Kredit abbezahlt ist, beenden die Schleife und geben die Dauer aus
                 print(f"Kredit abbezahlt, insgesamt dauert: {jahr-1} Jahre {monate} Monate")
                 return
-            netto_miete += miete * (1 - STEUER)
-            steuerersparnis += (monatliche_abschreibung + zinsen) * EINKOMMENSTEUER
-        cashflow = netto_miete - monatliche_rate * 12 + steuerersparnis
-        print(f"Ende Jahr: {jahr} kredit: {kredit}, Cashflow: {cashflow}, davon Zinsen: {zinsen_bezahlt}, Tilgung: {tilgung_bezahlt}, Steuerersparnis: {steuerersparnis}")
+        einkommen_aendern_pro_kopf = (miete*12 - zinsen_bezahlt - abschreibung) / 2
+        soll_steuer = einkommensteuer(gehaltA) + einkommensteuer(gehaltB)
+        echt_steuer = einkommensteuer(gehaltA + einkommen_aendern_pro_kopf) + einkommensteuer(gehaltB + einkommen_aendern_pro_kopf)
+        steuerersparnis = soll_steuer - echt_steuer
+        cashflow = miete * 12 - monatliche_rate * 12 + steuerersparnis
+        print(f"Ende Jahr: {jahr} kredit: {kredit:.1f}, Cashflow: {cashflow:.1f}, davon Miete: {miete*12:.1f}, Zinsen: {-zinsen_bezahlt:.1f}, Tilgung: {-tilgung_bezahlt:.1f}, Steuerersparnis: {steuerersparnis:.1f}")
         jahr += 1
 
 def main():
-    kaufpreis = 830000
-    eigenkapital = 100000
+    kaufpreis = 300000
+    eigenkapital = 30000
     zins = 0.0327
-    tilgung = 0.03
-    kaltmiete = 3000
-    baujar = 1900
-    haus = True # True für Haus, False für Wohnung
-    hausgeld = 0 # Nur für Wohnung
+    tilgung = 0.02
+    kaltmiete = 1100
+    baujar =1960
+    haus = False # True für Haus, False für Wohnung
+    hausgeld = 258 # Nur für Wohnung
     if haus:
         kaltmiete = kaltmiete
     else:
         kaltmiete = kaltmiete - hausgeld
     kredit = gesamtkosten(kaufpreis) - eigenkapital
+    ehemann_gehalt = 75000
+    ehefrau_gehalt = 70000
 
     print("Der Kreditbetrag beträgt: ", kredit)
     print("Die monatliche Rate beträgt: ", monatliche_rate(kredit, zins, tilgung))
     print("Die monatliche Miete beträgt: ", kaltmiete)
     print("Die jährige Abschreibung beträgt: ", abschreibung(kaufpreis, baujar))
     print("------------------------------------------------")
-    cashflow(kredit, kaltmiete, monatliche_rate(kredit, zins, tilgung), zins, abschreibung(kaufpreis, baujar))
+    cashflow(kredit, kaltmiete, monatliche_rate(kredit, zins, tilgung), zins, abschreibung(kaufpreis, baujar), ehemann_gehalt, ehefrau_gehalt)
 
 if __name__ == "__main__":
     main()
